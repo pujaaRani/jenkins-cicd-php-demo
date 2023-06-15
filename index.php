6b472fd52fd02b5dcef7acae78aa2297542aa54f
pujaaRani <130064924+pujaaRani@users.noreply.github.com>
2023-06-15 15:17:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>***Hello world php!!!!NEW Declarative pipeline project of php *** </p>'; ?> 
+ <?php echo '<p>***Hello world php proect  !!!!NEW Declarative pipeline project of php *** </p>'; ?> 
  </body>
 </html>
